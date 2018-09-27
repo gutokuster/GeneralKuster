014f7dd0c99e88cccf0812da030a5cce4d9740f3
Guto Küster <gutokuster@gmail.com>
2018-09-27 01:06:42 -0300

    Commit inicial.

---
+++ index.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Em Breve! Novo Site">
+    <meta name="author" content="gkuster">
+    <link rel="icon" href="assets/img/favicon.ico">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <title>Aguarde! Em breve teremos novidades.</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/embreve.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="site-wrapper">
+      <div class="site-wrapper-inner">
+        <div class="cover-container">
+          <div class="inner cover">
+            <h1 class="cover-heading">Estamos reformulando o nosso site!</h1>
+            <h2 class="cover-heading">Em breve teremos novidades!!</h2>
+            <p class="lead">
+              <img src="assets/img/tipico.png" alt="Restaurante Típico Parque Malwee - Jaraguá do Sul/SC"/>
+              <img src="assets/img/generalkuster.png" alt="Choperia General Küster - Balneário Piçarras/SC"/>
+            </p>
+            <br>
+            <p class="lead">
+                <h3>Contato: </h3>
+                       (47) 3345-1009 / Balneário Piçarras/SC <br>
+                       (47) 3376-0182 / Jaraguá do Sul/SC <br>
+            </p>
+          </div>
+          <div class="mastfoot">
+            <div class="inner">
+              <p>@2018 - Todos os direitos reservados. <a target="_blank" href="https://www.instagram.com/explore/locations/5945413/restaurante-tipico-parque-malwee/"><i class="fab fa-instagram"></i></a>  <a href="mailto:choperia@generalkuster.com.br"><i class="fas fa-at"></i></a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/jquery.min.js"><\/script>')</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
